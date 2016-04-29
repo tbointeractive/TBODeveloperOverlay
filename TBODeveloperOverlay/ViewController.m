09d0825bd1b488da8d05099a5ece7bd0e33291b9
Thorsten Stark <stark@tbointeractive.com>
2016-04-29 10:31:30 +0200

    setup project

---
+++ TBODeveloperOverlay/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TBODeveloperOverlay
+//
+//  Created by Thorsten Stark on 29.04.16.
+//  Copyright © 2016 TBO INTERACTIVE. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ TBODeveloperOverlayTests/TBODeveloperOverlayTests.m
@@ -0,0 +1,39 @@
+//
+//  TBODeveloperOverlayTests.m
+//  TBODeveloperOverlayTests
+//
+//  Created by Thorsten Stark on 29.04.16.
+//  Copyright © 2016 TBO INTERACTIVE. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TBODeveloperOverlayTests : XCTestCase
+
+@end
+
+@implementation TBODeveloperOverlayTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
