b83de569a64cd381b99177f2ba26e45f5c749171
Cornelius Horstmann <horstmann@tbointeractive.com>
2016-05-20 20:20:23 +0200

    automatically adding the done button to all pushed plugins

---
+++ TBODeveloperOverlay/TBODeveloperOverlay/TBOModalNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  TBOModalNavigationController.h
+//  TBODeveloperOverlay
+//
+//  Created by Cornelius Horstmann on 20.05.16.
+//  Copyright © 2016 TBO INTERACTIVE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBOModalNavigationController : UINavigationController
+
+@end
