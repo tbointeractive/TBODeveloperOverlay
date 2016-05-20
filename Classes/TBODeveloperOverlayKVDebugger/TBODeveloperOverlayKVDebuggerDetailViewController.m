1f1d79662396be3b95a467920b6cfcb577ba44b4
Thorsten Stark <stark@tbointeractive.com>
2016-05-20 15:05:01 +0200

    detail view configures itself depending of value type and can return changed value to datasource

--- Classes/TBODeveloperOverlayKVDebugger/TBODeveloperOverlayKVDebuggerDetailViewController.m
+++ Classes/TBODeveloperOverlayKVDebugger/TBODeveloperOverlayKVDebuggerDetailViewController.m
@@ -8,17 +8,26 @@
 
 #import "TBODeveloperOverlayKVDebuggerDetailViewController.h"
 
+typedef enum {
+    ValueTypeUnknown,
+    ValueTypeString,
+    ValueTypeNumber,
+    ValueTypeBool
+} ValueType;
+
 @interface TBODeveloperOverlayKVDebuggerDetailViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *descriptionLabel;
 @property (weak, nonatomic) IBOutlet UISwitch *valueSwitch;
 @property (weak, nonatomic) IBOutlet UITextView *valueTextView;
+@property (weak, nonatomic) IBOutlet UITextField *valueTextField;
 
 @property (strong, nonatomic, readwrite) UIBarButtonItem *saveButton;
 
 @property (strong, nonatomic, readwrite) NSIndexPath *indexPath;
 @property (nonatomic, strong) id<TBODeveloperOverlayKVDebuggerDatasource> datasource;
 
+@property ValueType valueType;
 @end
 
 @implementation TBODeveloperOverlayKVDebuggerDetailViewController
@@ -34,16 +43,68 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    if ([self.datasource isEditableForIndexPath:self.indexPath]) {
+    BOOL isEditable = [self.datasource isEditableForIndexPath:self.indexPath];
+    if (isEditable) {
         self.navigationItem.rightBarButtonItem = self.saveButton;
     }
-    
+    self.title = [self.datasource keyForIndexPath:self.indexPath];
     self.titleLabel.text = [self.datasource keyForIndexPath:self.indexPath];
+    self.descriptionLabel.text = [self.datasource descriptionForIndexPath:self.indexPath] ? : @"no description available";
     
+    id value = [self.datasource valueForIndexPath:self.indexPath];
+    if ([value isKindOfClass:[NSString class]]) {
+        self.valueTextView.text = (NSString *)value;
+        self.valueTextView.hidden = NO;
+        self.valueTextView.editable = isEditable;
+        self.valueType = ValueTypeString;
+    } else if (value == (void *)kCFBooleanFalse || value == (void *)kCFBooleanTrue) {
+        [self.valueSwitch setOn:((NSNumber *)value).boolValue];
+        self.valueSwitch.hidden = NO;
+        self.valueSwitch.enabled = isEditable;
+        self.valueType = ValueTypeBool;
+    } else if ([value isKindOfClass:[NSNumber class]]) {
+        self.valueTextField.text = ((NSNumber *)value).stringValue;
+        self.valueTextField.hidden = NO;
+        self.valueTextField.enabled = isEditable;
+        self.valueType = ValueTypeNumber;
+    } else {
+        self.valueTextView.text = [value description];
+        self.valueTextView.hidden = NO;
+        self.valueTextView.editable = isEditable;
+        self.valueType = ValueTypeUnknown;
+    }
 }
 
 - (void)saveButtonTapped {
+    id newValue = [self currentValueFromInput];
+    [self.datasource didChangeValue:newValue atIndexPath:self.indexPath];
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (id)currentValueFromInput {
+    switch (self.valueType) {
+        case ValueTypeBool:
+            return @(self.valueSwitch.isOn);
+        case ValueTypeNumber: {
+            NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
+            numberFormatter.numberStyle = NSNumberFormatterDecimalStyle;
+            return [numberFormatter numberFromString:self.valueTextField.text];
+        }
+        case ValueTypeString:
+            return self.valueTextView.text;
+        default:
+            return self.valueTextView.text;
+    }
+}
+
+#pragma mark TextField Delegate
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    NSCharacterSet *numbersOnly = [NSCharacterSet characterSetWithCharactersInString:@"0123456789.,"];
+    NSCharacterSet *characterSetFromTextField = [NSCharacterSet characterSetWithCharactersInString:textField.text];
     
+    BOOL stringIsValid = [numbersOnly isSupersetOfSet:characterSetFromTextField];
+    return stringIsValid;
 }
 
 #pragma mark lazy instatiation
