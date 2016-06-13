5ffee681958f15af01a3f243421eb21915a5df14
Cornelius Horstmann <horstmann@tbointeractive.com>
2016-06-13 16:01:10 +0200

    moved plugin documentation to header files

--- Classes/TBODeveloperOverlayFileInspector/TBODeveloperOverlayFileInspectorViewController.h
+++ Classes/TBODeveloperOverlayFileInspector/TBODeveloperOverlayFileInspectorViewController.h
@@ -8,6 +8,21 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ * The TBODeveloperOverlayFileInspectorViewController displays a folder structure to inspect files that are locally saved.
+ *
+ * ## Usage
+ * The TBODeveloperOverlayFileInspectorViewController has to be inititalized with one ore more base urls.
+ * If the -initWithBaseUrl: method is used, the first screen displays already the content of that folder. If the -initWithBaseUrls: method is used, the first screen lists those folders. Tapping on them makes it possible to drill down.
+ *
+ * Per default the title is the last path component of the one baseurl. To use another title you can override the `title`-property.
+ *
+ * ## Example
+ *     NSURL baseUrl = [[NSURL fileURLWithPath:[NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) firstObject]] URLByDeletingLastPathComponent];
+ *     TBODeveloperOverlayFileInspectorViewController *fileInspector = [[TBODeveloperOverlayFileInspectorViewController alloc] initWithBaseUrl:baseUrl];
+ *     fileInspector.title = @"File Inspector";
+ *
+ */
 @interface TBODeveloperOverlayFileInspectorViewController : UITableViewController
 
 /**
@@ -16,7 +31,7 @@
  *
  * @param baseUrl should be a local readable url
  */
-- (instancetype)initWithBaseUrl:(NSURL * _Nonnull)baseUrl;
+- (instancetype)initWithBaseUrl:(NSURL *_Nonnull)baseUrl;
 
 /**
  * Use this method if you want to inspect multiple folders
@@ -24,6 +39,6 @@
  *
  * @param baseUrls should be an array of local readable urls
  */
-- (instancetype)initWithBaseUrls:(NSArray<NSURL *> * _Nonnull)baseUrls;
+- (instancetype)initWithBaseUrls:(NSArray<NSURL *> *_Nonnull)baseUrls;
 
 @end
