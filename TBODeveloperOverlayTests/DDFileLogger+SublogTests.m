f9d9380e721b2e4bd0a205ea2de9004a07da0660
Bernhard Eiling <eiling@tbointeractive.com>
2016-06-15 14:41:59 +0200

    added test covering off by one error

--- TBODeveloperOverlayTests/DDFileLogger+SublogTests.m
+++ TBODeveloperOverlayTests/DDFileLogger+SublogTests.m
@@ -8,10 +8,17 @@
 
 #import <XCTest/XCTest.h>
 #import "DDFileLogger+Sublog.h"
+#import "TBOLoggerSetupHelper.h"
 
 @interface DDFileLogger (Sublog)
 
 - (NSString *)lastLogMessagesLimitedToCharacterCount:(NSUInteger)maxCharacterCount fromFilePaths:(NSArray<NSString *> *)sortedLogFilePaths;
+
+@end
+
+@interface TBOLoggerSetupHelper ()
+
++ (DDFileLogger *)fileLogger;
 
 @end
 
@@ -26,23 +33,29 @@
 
 - (void)setUp {
     [super setUp];
-    self.fileLogger = [DDFileLogger new];
+    [TBOLoggerSetupHelper setup];
+    self.fileLogger = [TBOLoggerSetupHelper fileLogger];
     self.filePaths = [NSMutableArray new];
-    
     NSFileManager *fileManager = [NSFileManager defaultManager];
-    NSArray <NSData *> *testLogs = @[[@"first log content" dataUsingEncoding:NSUTF8StringEncoding],
+    NSArray <NSData *> *testLogs = @[[@"start first log content" dataUsingEncoding:NSUTF8StringEncoding],
                                      [@"second log content" dataUsingEncoding:NSUTF8StringEncoding],
-                                     [@"third log content" dataUsingEncoding:NSUTF8StringEncoding]];
+                                     [@"third log content end" dataUsingEncoding:NSUTF8StringEncoding]];
     [testLogs enumerateObjectsUsingBlock:^(NSData *_Nonnull logData, NSUInteger idx, BOOL *_Nonnull stop) {
         NSString *filePath = [NSTemporaryDirectory() stringByAppendingString:@(idx).stringValue];
         [self.filePaths addObject:filePath];
+        [fileManager removeItemAtPath:filePath error:nil];
         [fileManager createFileAtPath:filePath contents:logData attributes:nil];
     }];
 }
 
 - (void)testGettingLogString {
     NSString *retrievedLogs = [self.fileLogger lastLogMessagesLimitedToCharacterCount:5000 fromFilePaths:self.filePaths];
-    
+    XCTAssertEqualObjects(@"start first log contentsecond log contentthird log content end", retrievedLogs);
+}
+
+- (void)testGettinglLast10Chars {
+    NSString *retrievedLogs = [self.fileLogger lastLogMessagesLimitedToCharacterCount:10 fromFilePaths:self.filePaths];
+    XCTAssertEqualObjects(@"ontent end", retrievedLogs);
 }
 
 @end
