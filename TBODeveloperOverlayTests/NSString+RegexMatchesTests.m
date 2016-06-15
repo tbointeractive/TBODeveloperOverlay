c6898831babea2690929d53ea869979fc32d2f86
Bernhard Eiling <eiling@tbointeractive.com>
2016-06-15 17:26:25 +0200

    moar tests

--- TBODeveloperOverlayTests/NSString+RegexMatchesTests.m
+++ TBODeveloperOverlayTests/NSString+RegexMatchesTests.m
@@ -18,14 +18,46 @@
 
 NSString *testString = @"this is a string\nwhich includes a couple\nof newlines";
 
-- (void)testMatchingSubstring {
+- (void)testRegexMatchingSubstring {
     NSRegularExpression *regularExpression = [NSRegularExpression regularExpressionWithPattern:@"which includes" options:kNilOptions error:nil];
     NSString *subString = [testString substringThatMatchesRegex:regularExpression];
     XCTAssertEqualObjects(@"which includes a couple", subString);
 }
 
-- (void)testNotMatchingSubstring {
+- (void)testRegexNotMatchingSubstring {
     NSRegularExpression *regularExpression = [NSRegularExpression regularExpressionWithPattern:@"not matching" options:kNilOptions error:nil];
+    NSString *subString = [testString substringThatMatchesRegex:regularExpression];
+    XCTAssertEqualObjects(@"", subString);
+}
+
+- (void)testAnyRegexMatchingSubstring {
+    NSRegularExpression *regularExpression1 = [NSRegularExpression regularExpressionWithPattern:@"this is" options:kNilOptions error:nil];
+    NSRegularExpression *regularExpression2 = [NSRegularExpression regularExpressionWithPattern:@"which includes" options:kNilOptions error:nil];
+    NSString *subString = [testString substringThatMatchesAnyRegex:@[regularExpression1, regularExpression2]];
+    XCTAssertEqualObjects(@"this is a string\nwhich includes a couple", subString);
+}
+
+- (void)testNotAnyRegexMatchingSubstring {
+    NSRegularExpression *regularExpression1 = [NSRegularExpression regularExpressionWithPattern:@"not matching" options:kNilOptions error:nil];
+    NSRegularExpression *regularExpression2 = [NSRegularExpression regularExpressionWithPattern:@"not matching as well" options:kNilOptions error:nil];
+    NSString *subString = [testString substringThatMatchesAnyRegex:@[regularExpression1, regularExpression2]];
+    XCTAssertEqualObjects(@"", subString);
+}
+
+- (void)testAnyRegexMatchingWithFalseTypes {
+    NSRegularExpression *regularExpression = [NSRegularExpression regularExpressionWithPattern:@"which includes" options:kNilOptions error:nil];
+    NSString *subString = [testString substringThatMatchesAnyRegex:@[regularExpression, @1]];
+    XCTAssertEqualObjects(@"which includes a couple", subString);
+}
+
+- (void)testRegexWithNewline {
+    NSRegularExpression *regularExpression = [NSRegularExpression regularExpressionWithPattern:@"string\nwhich" options:kNilOptions error:nil];
+    NSString *subString = [testString substringThatMatchesRegex:regularExpression];
+    XCTAssertEqualObjects(@"", subString);
+}
+
+- (void)testLongRegexDescribingTargetStringTwice {
+    NSRegularExpression *regularExpression = [NSRegularExpression regularExpressionWithPattern:@"this is a string\nwhich includes a couple\nof newlinesthis is a string\nwhich includes a couple\nof newlines" options:kNilOptions error:nil];
     NSString *subString = [testString substringThatMatchesRegex:regularExpression];
     XCTAssertEqualObjects(@"", subString);
 }
