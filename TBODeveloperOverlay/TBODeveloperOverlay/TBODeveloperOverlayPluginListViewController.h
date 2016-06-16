8cb9f5fae9bdc5224738eaaa8120245356e2158b
Thorsten Stark <stark@tbointeractive.com>
2016-06-16 14:45:00 +0200

    documenation for TBODeveloperOverlayPluginListViewController

--- TBODeveloperOverlay/TBODeveloperOverlay/TBODeveloperOverlayPluginListViewController.h
+++ TBODeveloperOverlay/TBODeveloperOverlay/TBODeveloperOverlayPluginListViewController.h
@@ -7,9 +7,26 @@
 //
 
 #import <UIKit/UIKit.h>
+/**
+ * The TBODeveloperOverlayPluginListViewController gives an overview of all plugins it is initiated with. It is designed to be used in modally presented ViewController. Therefore it has a "Done"-Button which dismisses the ViewController when your done.
+ *
+ * ## Usage
+ * TBODeveloperOverlayPluginListViewController has to be instantiated with an array of plugins. Then it can be set as the RootViewController of TBOModalNavigationController or another UINavigationController.
+ *
+ * `
+ * NSArray *plugins = @[kvDebuggerViewController, userDefaultsInspector, logger, fileInspector];
+ * TBODeveloperOverlayPluginListViewController* listViewController = [[TBODeveloperOverlayPluginListViewController alloc] initWithPlugins:plugins];
+ * TBOModalNavigationController *developerOverlay = [[TBOModalNavigationController alloc] initWithRootViewController:listViewController];
+ * `
+ */
 
 @interface TBODeveloperOverlayPluginListViewController : UITableViewController
 
+/**
+ * Instantiates the TBODeveloperOverlayPluginListViewController with an array of plugins.
+ * @param plugins A NSArray containing the plugins to be displayed.
+ * @return An instance of TBODeveloperOverlayPluginListViewController.
+ */
 - (instancetype)initWithPlugins:(NSArray *)plugins;
 
 @end
